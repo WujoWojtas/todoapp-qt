bf5585d29e60f035c50490f14cfdfbef2dc1da20
WujoWojtas <wujowojtas@gmail.com>
2015-03-27 00:39:50 +0100

    translations

--- TodoApp/TodoApp.pro
+++ TodoApp/TodoApp.pro
@@ -18,3 +18,5 @@ SOURCES += main.cpp\
 HEADERS  += mainwindow.h
 
 FORMS    += mainwindow.ui
+
+TRANSLATIONS += todoapp_pl.ts
---
+++ TodoApp/todoapp_pl.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>Todo App</source>
+        <translation>Lista zadań</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="38"/>
+        <source>Add item</source>
+        <translation>Dodaj zadanie</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="45"/>
+        <source>Delete selected</source>
+        <translation>Usuń zaznaczone</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="52"/>
+        <source>Clear all</source>
+        <translation>Skasuj wszystkie</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="63"/>
+        <source>Load from JSON</source>
+        <translation>Wczytaj dane z JSON</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="70"/>
+        <source>Export to JSON</source>
+        <translation>Eksportuj dane do JSON</translation>
+    </message>
+</context>
+</TS>
